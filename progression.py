56ee3697a5e806271c214c2a9f09d02cdc549a3e
Tyler John Haden <tylerjohnhaden@gmail.com>
2018-03-31 02:11:59 -0400

    a progression of quines

---
+++ progression.py
@@ -0,0 +1,13 @@
+print('{0}{0!r}))'.format("print('{0}{0!r}))'.format("))
+print('{0}{0!r}, {1!r}{1}'.format("print('{0}{0!r}, {1!r}{1}'.format(", '))'))
+print('{0}{0!r}, {1!r}, {2!r}{2}'.format("print('{0}{0!r}, {1!r}, {2!r}{2}'.format(", 'asdf', '))'))
+print("{0}{0!r}, ''{1!r}'', {2!r}{2}".format('print("{0}{0!r}, \'\'{1!r}\'\', {2!r}{2}".format(', '''asdf''', '))'))
+print("{0}{0!r}, '''{1}''', {2!r}{2}".format('print("{0}{0!r}, \'\'\'{1}\'\'\', {2!r}{2}".format(', '''
+
+                                o_o
+
+
+
+
+
+''', '))'))
